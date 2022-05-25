a09c0324978af22685fcd0afbf4cd567ba589171
Nick Wagner <Nick.Wagner@vitalyst.com>
2022-05-25 11:40:20 -0400

    Add TeamsSSOCookie

---
+++ TeamsSSOCookie/src/TeamsSSOCookie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuthExampleYT2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
